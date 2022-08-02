3688e25a774233daa99aac11b7207ff714014910
oulige <103018846+oulige@users.noreply.github.com>
2022-08-02 13:40:46 +0800

    Add files via upload

---
+++ txl2.js
@@ -0,0 +1,124 @@
+{
+"code":0,
+"data":
+[ {
+            "id": "1",
+            "name": "学校管理机构",
+             "persons": [ {
+                "id": "persons1",
+                "name": "党委组织部",
+                "phone": "",
+                "email": "ltzy_zzb@ltzy.edu.cn",
+                "telephone": "0772-3698310"
+            }, {
+                "id": "persons2",
+                "name": "党委办公室",
+                "phone": "",
+                "email": "ltzy_db@ltzy.edu.cn",
+                "telephone": "0772-3679166"
+            }, {
+                "id": "persons3",
+                "name": "党委宣传部",
+                "phone": "",
+                "email": "ltzyxc@163.com",
+                "telephone": "0772-3698220"
+            }, {
+                "id": "persons4",
+                "name": "人事处",
+                "phone": "",
+                "email": "ltzyrsc@163.com",
+                "telephone": "0772-3679185"
+            }, {
+                "id": "persons5",
+                "name": "师资发展科",
+                "phone": "",
+                "email": "",
+                "telephone": "0772-3698225"
+            }, {
+                "id": "persons6",
+                "name": "学校办公室",
+                "phone": "",
+                "email": "ltzy3000@163.com",
+                "telephone": "0772-3698666"
+            } , {
+                "id": "persons7",
+                "name": "教务处",
+                "phone": "",
+                "email": "ltzy_jwc@ltzy.edu.cn",
+                "telephone": "0772-398308"
+            }, {
+                "id": "persons8",
+                "name": "学生处",
+                "phone": "",
+                "email": "ltzyxsc2021@163.com",
+                "telephone": "0772-3679126"
+            }, {
+                "id": "persons9",
+                "name": "保卫处",
+                "phone": "",
+                "email": "ltzybwc@163.com",
+                "telephone": "0772-3679110"
+            }, {
+                "id": "persons10",
+                "name": "科技与图文信息处",
+                "phone": "",
+                "email": "ltzy_kxc@ltzy.edu.cn",
+                "telephone": "0772-3679018"
+            }, {
+                "id": "persons11",
+                "name": "后勤保障处",
+                "phone": [ "0772-3679096" ],
+                "email": "ltzyhqc@163.com",
+                "telephone": "0772-3698253"
+            }, {
+                "id": "persons12",
+                "name": "招生就业处",
+                "phone": [ "0772-3698188","0772-3698108" ],
+                "email": "ltzy_zjc@163.com",
+                "telephone": "0772-3698198"
+            }, {
+                "id": "persons13",
+                "name": "计财处",
+                "phone": "",
+                "email": "ltzy_jcc@ltzy.edu.cn",
+                "telephone": "0772-3698288"
+            }, {
+                "id": "persons14",
+                "name": "纪检监察室",
+                "phone": [ "0772-3679045" ],
+                "email": "ltzyjjjc@163.com",
+                "telephone": "0772-3698221"
+            }, {
+                "id": "persons15",
+                "name": "工会",
+                "phone": "",
+                "email": "1142120293@qq.com",
+                "telephone": "0772-3698187"
+            }, {
+                "id": "persons16",
+                "name": "离退休工作办公室",
+                "phone": [ "0772-3698398" ],
+                "email": "1142120293@qq.com",
+                "telephone": "0772-3698153"
+            }, {
+                "id": "persons17",
+                "name": "团委",
+                "phone": "",
+                "email": "0772-3679102",
+                "telephone": "ltzytw@126.com"
+            }, {
+                "id": "persons18",
+                "name": "质量管理办公室",
+                "phone": "",
+                "email": "ltzyzlb@163.com",
+                "telephone": "0772-3679181"
+            }, {
+                "id": "persons19",
+                "name": "招标采购办公室",
+                "phone": "",
+                "email": "1579391393@qq.com",
+                "telephone": "0772-3698361"
+            }]
+        } ],
+"message":"获取成功"
+}
