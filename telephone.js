3b9293a682bc485fb193215add40f9438354add6
oulige <103018846+oulige@users.noreply.github.com>
2022-08-02 10:04:01 +0800

    Add files via upload

---
+++ telephone.js
@@ -0,0 +1,84 @@
+{
+"code":0,
+"data":
+[ {
+            "id": "1",
+            "name": "通信信号学院",
+            " persons": [ {
+                "id": "persons1",
+                "name": "张剑伟",
+                "phone": [ "13480225772" ],
+                "email": "zhangjianwei30@163.com",
+                "telephone": "020-87236718"
+            }, {
+                "id": "persons2",
+                "name": "朱亮标",
+                "phone": [ "18620066588" ],
+                "email": "zhulb@ceprei.com",
+                "telephone": "020-87236858"
+            }, {
+                "id": "persons87",
+                "name": "葛智君",
+                "phone": [ "13570481618" ],
+                "email": "",
+                "telephone": "020-87021354"
+            } ]
+        }, {
+            "id": "2",
+            "name": "智能制造学院",
+            " persons": [ {
+                "id": "persons5",
+                "name": "陈海鹏",
+                "phone": [ "18027398660", "13660290309" ],
+                "email": "chenhaipeng@163.com",
+                "telephone": "020-87236772"
+            }, {
+                "id": "persons81",
+                "name": "刘舜鑫",
+                "phone": [ "18819490001" ],
+                "email": "",
+                "telephone": "020-87236029"
+            }, {
+                "id": "persons3",
+                "name": "赵立新",
+                "phone": [ "13249649791", "18688886557" ],
+                "email": "18698875536@163.com",
+                "telephone": "020-37206036"
+            }, {
+                "id": "persons4",
+                "name": "谢霖",
+                "phone": [ "18664627388" ],
+                "email": "xielin9602@tom.com",
+                "telephone": "020-87236772"
+            } ]
+        }, {
+            "id": "3",
+            "name": "通信与物联网学院",
+            " persons": [ {
+                "id": "persons12",
+                "name": "王妙",
+                "phone": [ "18620218679" ],
+                "email": "2242665177@qq.com",
+                "telephone": "020-87237925"
+            }, {
+                "id": "persons11",
+                "name": "鄂湄",
+                "phone": [ "13699702273" ],
+                "email": "",
+                "telephone": "020-87237925"
+            }, {
+                "id": "persons13",
+                "name": "王丽媛",
+                "phone": [ "18620595936" ],
+                "email": "893193678@qq.com",
+                "telephone": "020-87237925"
+            }, {
+                "id": "persons7",
+                "name": "伊贤周",
+                "phone": [ "15017543061" ],
+                "email": "yixianzhou@163.com",
+                "telephone": "020-87237925"
+            } ]
+        } ],
+"message":"获取成功"
+}
