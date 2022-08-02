3bfd194757ffb8a5d0aabfd81b86991ccca39eda
oulige <103018846+oulige@users.noreply.github.com>
2022-08-02 13:37:09 +0800

    Update txl.js

--- txl.js
+++ txl.js
@@ -7,61 +7,61 @@
              "persons": [ {
                 "id": "persons1",
                 "name": "党委组织部",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "ltzy_zzb@ltzy.edu.cn",
                 "telephone": "0772-3698310"
             }, {
                 "id": "persons2",
                 "name": "党委办公室",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "ltzy_db@ltzy.edu.cn",
                 "telephone": "0772-3679166"
             }, {
                 "id": "persons3",
                 "name": "党委宣传部",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "ltzyxc@163.com",
                 "telephone": "0772-3698220"
             }, {
                 "id": "persons4",
                 "name": "人事处",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "ltzyrsc@163.com",
                 "telephone": "0772-3679185"
             }, {
                 "id": "persons5",
                 "name": "师资发展科",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "",
                 "telephone": "0772-3698225"
             }, {
                 "id": "persons6",
                 "name": "学校办公室",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "ltzy3000@163.com",
                 "telephone": "0772-3698666"
             } , {
                 "id": "persons7",
                 "name": "教务处",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "ltzy_jwc@ltzy.edu.cn",
                 "telephone": "0772-398308"
             }, {
                 "id": "persons8",
                 "name": "学生处",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "ltzyxsc2021@163.com",
                 "telephone": "0772-3679126"
             }, {
                 "id": "persons9",
                 "name": "保卫处",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "ltzybwc@163.com",
                 "telephone": "0772-3679110"
             }, {
                 "id": "persons10",
                 "name": "科技与图文信息处",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "ltzy_kxc@ltzy.edu.cn",
                 "telephone": "0772-3679018"
             }, {
@@ -79,7 +79,7 @@
             }, {
                 "id": "persons13",
                 "name": "计财处",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "ltzy_jcc@ltzy.edu.cn",
                 "telephone": "0772-3698288"
             }, {
@@ -91,7 +91,7 @@
             }, {
                 "id": "persons15",
                 "name": "工会",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "1142120293@qq.com",
                 "telephone": "0772-3698187"
             }, {
@@ -103,19 +103,19 @@
             }, {
                 "id": "persons17",
                 "name": "团委",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "0772-3679102",
                 "telephone": "ltzytw@126.com"
             }, {
                 "id": "persons18",
                 "name": "质量管理办公室",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "ltzyzlb@163.com",
                 "telephone": "0772-3679181"
             }, {
                 "id": "persons19",
                 "name": "招标采购办公室",
-                "phone": [ "" ],
+                "phone": "",
                 "email": "1579391393@qq.com",
                 "telephone": "0772-3698361"
             }]
