1b155e0e1c5033272c16e9aeac05a9658b13951b
oulige <103018846+oulige@users.noreply.github.com>
2022-08-15 16:41:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
       "url": ""
     }
   ],
-  "wxurl": "https://pkwx.ltzy.edu.cn/wx/scores?code=Z716sKgtybFfXomWtvx1QXjkepRro&cardNumber=",
+  "cjurl": "https://pkwx.ltzy.edu.cn/wx/scores?code=Z716sKgtybFfXomWtvx1QXjkepRro&cardNumber=",
+  "kburl": "https://pkwx.ltzy.edu.cn/wx/corseuid?code=Z716sKgtybFfXomWtvx1QXjkepRro&zc=",
   "xiaoli":"备注:\n1. 9.06-9.08: 新生报到\n2. 9.10-9.10: 教师节、中秋节\n3. 10.01 - 10.07: 国庆节\n4. 11.05 - 11.06: 校运动会\n5. 1.01 - 1.01: 元旦\n6. 1.9 - 1.10: 期末统考"
 }
