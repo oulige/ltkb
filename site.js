cdf0bc1d1dc7ea4b67ff13a2cbed8a315f1feccb
oulige <103018846+oulige@users.noreply.github.com>
2022-08-24 20:11:32 +0800

    Add files via upload

---
+++ site.js
@@ -0,0 +1,43 @@
+[
+  {
+    "applicationName": "学校官网",
+    "url": "https://www.ltzy.edu.cn/",
+    "description": "学校官网",
+    "imageURL": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i43yiei8j2046046aa3.jpg",
+    "enabled": true,
+    "createdAt": "2022-07-18T08:19:04.618Z"
+  },
+  {
+    "applicationName": "办事大厅",
+    "url": "https://vpn.ltzy.edu.cn/",
+    "description": "办事大厅（外网）",
+    "imageURL": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i43yiei8j2046046aa3.jpg",
+    "enabled": true,
+    "createdAt": "2022-07-18T08:49:18.829Z"
+  },
+  {
+    "applicationName": "返校系统",
+    "url": "https://xg.ltzy.edu.cn/SPCP/Web/",
+    "description": "返校申请",
+    "imageURL": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i43yiei8j2046046aa3.jpg",
+    "enabled": true,
+    "createdAt": "2022-07-18T08:49:18.829Z"
+  },
+  {
+    "applicationName": "报修系统",
+    "url": "https://ehall.ltzy.edu.cn/psfw/sys/swmssbxapp/",
+    "description": "宿舍水电报修",
+    "imageURL": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i43yiei8j2046046aa3.jpg",
+    "enabled": true,
+    "createdAt": "2022-07-18T08:49:18.829Z"
+  },
+  {
+    "applicationName": "食堂投诉",
+    "url": "https://wxtsjy.ltzy.edu.cn/",
+    "description": "食堂卫生投诉建议",
+    "imageURL": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i43yiei8j2046046aa3.jpg",
+    "enabled": true,
+    "createdAt": "2022-07-18T08:49:18.829Z"
+  }
+  
+]
