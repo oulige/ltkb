0e27ba3c373dc1446fe420412246a80c4486ffe3
oulige <103018846+oulige@users.noreply.github.com>
2022-08-25 13:16:55 +0800

    Add files via upload

---
+++ post3.js
@@ -0,0 +1,13 @@
+{
+  "code": "0",
+  "imgList": [
+    {"pic":"https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3mzvvwgj20tz16athk.jpg","name":""},
+      {
+        "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3na4klfj20tz16lk33.jpg", "name": "" },
+      {
+        "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3nju4h7j20tz16i49d.jpg", "name": "入学须知" },
+      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3o4p3tjj20tz16i7bz.jpg", "name": "专业收费表" },
+      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3vw7tohj20u017e7de.jpg", "name": "作息时间表"  },
+      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3u1sw20j20k80nidjl.jpg", "name": "学校地图" }
+  ]
+}
