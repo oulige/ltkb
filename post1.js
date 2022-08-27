83389eee9029673adbb5813f9f6cf2092f58658e
oulige <103018846+oulige@users.noreply.github.com>
2022-08-27 16:15:59 +0800

    Update post1.js

--- post1.js
+++ post1.js
@@ -1,13 +1,8 @@
-{
-  "code": "0",
-  "imgList": [
-    {"pic":"https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3mzvvwgj20tz16athk.jpg","name":""},
-      {
-        "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3na4klfj20tz16lk33.jpg", "name": "" },
-      {
-        "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3nju4h7j20tz16i49d.jpg", "name": "入学须知" },
-      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3o4p3tjj20tz16i7bz.jpg", "name": "专业收费表" },
-      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3vw7tohj20u017e7de.jpg", "name": "作息时间表"  },
-      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3u1sw20j20k80nidjl.jpg", "name": "学校地图" }
-  ]
-}
+
+{"status":1,"msg":"success","data":{"title":"柳州铁道职业技术学院2022年专升本拟推荐学生名单公示","src":"\u7ea2\u661f\u65b0\u95fb",
+  "pic":"https:\/\/n.sinaimg.cn\/ent\/transform\/500\/w300h200\/20220825\/26bc-2b8c22adc6ba9de11539bc11662660e4.jpg",
+    "content":"\n\n根据《自治区教育厅关于做好2022年全区普通专升本招生工作的通知》（桂教高教〔2022〕17号）文件精神，我校组织开展了2022年高职升本工作，制定了实施细则，并对年级专业排名前60％学生名单予以公示，在各学院选拔和推荐基础上，经审核，现将我校2022年高职升本拟推荐的黄孙娇等861人名单予以公示，公示期为5个工作日（6月1日-8日），欢迎广大师生监督，如发现在选拔推荐中出现信息错误或弄虚作假等现象，可通过拨打电话或发送OA邮件等形式告知教务处，联系人：付振中，联系电话：0772-3698192，感谢您的合作！<\/p>\n<p class=\"art_p\">注意：我校2022年高职升本学生最终名单以教育厅公布名单为准。<\/p>\n\r\n\t\t\t\t\t\t\t\t\t\t<figure class=\"art_img_mini j_p_gallery\">\r\n\t\t\t\t\t\t\t\t\t\t\t<img class=\"art_img_mini_img j_fullppt_cover\"  w=\"550\" h=\"511\" src='https:\/\/n.sinaimg.cn\/ent\/transform\/261\/w550h511\/20220825\/7385-bb12f31763f47167315c7caa3d78e293.jpg'  \/>\r\n\t\t\t\t\t\t\t\t\t\t\t\r\n\t\t\t\t\t\t\t\t\t\t<\/figure>\r\n\t\t\t\t\t\t\t\t\t\n<p class=\"art_p\">","url":"https:\/\/ent.sina.cn\/star\/tv\/2022-08-25\/detail-imizirav9632388.d.html?cre=tianyi&mod=went&loc=0&r=-1&rfunc=96&tj=cxvertical_wap_went&tr=73",
+      "pdf":[
+    {"url":"https://jwweb.ltzy.edu.cn/lztdjw/attached/file/20220601/1654079876012049585.pdf","name":"柳州铁道职业技术学院2022年专升本拟推荐学生名单.pdf"},
+        
+  ]}}
