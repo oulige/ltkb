234eacb9bf30bb4abfb16adb824e8b72de4847fc
oulige <103018846+oulige@users.noreply.github.com>
2022-08-31 17:49:03 +0800

    Update xsxz.js

--- xsxz.js
+++ xsxz.js
@@ -5,8 +5,8 @@
       {
         "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3na4klfj20tz16lk33.jpg", "name": "" },
       {
-        "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3nju4h7j20tz16i49d.jpg", "name": "入学须知" },
-      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3o4p3tjj20tz16i7bz.jpg", "name": "专业收费表" },
+        "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3nju4h7j20tz16i49d.jpg", "name": "" },
+      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3o4p3tjj20tz16i7bz.jpg", "name": "" },
       { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5q38nijlxj20sk178gqa.jpg", "name": ""  },
       { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5q33a3d9qj20sm17ogqo.jpg", "name": ""  },
       { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5q3618x9tj20ss1coq99.jpg", "name": ""  },
