4cb25fb782538a3bc5f043f1196ef14dbe307ef9
oulige <103018846+oulige@users.noreply.github.com>
2022-08-31 17:46:29 +0800

    Update xsxz.js

--- xsxz.js
+++ xsxz.js
@@ -7,7 +7,9 @@
       {
         "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3nju4h7j20tz16i49d.jpg", "name": "入学须知" },
       { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3o4p3tjj20tz16i7bz.jpg", "name": "专业收费表" },
-      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3vw7tohj20u017e7de.jpg", "name": "作息时间表"  },
+      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5q38nijlxj20sk178gqa.jpg", "name": ""  },
+      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5q33a3d9qj20sm17ogqo.jpg", "name": ""  },
+      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5q3618x9tj20ss1coq99.jpg", "name": ""  },
       { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3u1sw20j20k80nidjl.jpg", "name": "学校地图" }
   ]
 }
