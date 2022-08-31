be53f058dc3664d492c83eb86524a4047cbfb4ab
oulige <103018846+oulige@users.noreply.github.com>
2022-08-31 23:08:40 +0800

    Update site.js

--- site.js
+++ site.js
@@ -15,6 +15,14 @@
     "enabled": true,
     "createdAt": "2022-07-18T08:49:18.829Z"
   },
+   {
+    "applicationName": "学校缴费系统",
+    "url": "https://pag.ltzy.edu.cn/",
+    "description": "学杂费在线缴费系统",
+    "imageURL": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i43yiei8j2046046aa3.jpg",
+    "enabled": true,
+    "createdAt": "2022-07-18T08:49:18.829Z"
+  },
   {
     "applicationName": "返校系统",
     "url": "https://xg.ltzy.edu.cn/SPCP/Web/",
@@ -27,14 +35,6 @@
     "applicationName": "报修系统",
     "url": "https://ehall.ltzy.edu.cn/psfw/sys/swmssbxapp/*default/index.do#/",
     "description": "宿舍水电报修",
-    "imageURL": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i43yiei8j2046046aa3.jpg",
-    "enabled": true,
-    "createdAt": "2022-07-18T08:49:18.829Z"
-  },
-  {
-    "applicationName": "食堂投诉",
-    "url": "https://wxtsjy.ltzy.edu.cn/",
-    "description": "食堂卫生投诉建议",
     "imageURL": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i43yiei8j2046046aa3.jpg",
     "enabled": true,
     "createdAt": "2022-07-18T08:49:18.829Z"
