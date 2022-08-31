e9bfc734a407406d8c37c437c71c1e0f14589e0a
oulige <103018846+oulige@users.noreply.github.com>
2022-08-31 20:53:52 +0800

    Update txl2.js

--- txl2.js
+++ txl2.js
@@ -1,124 +1,457 @@
 {
-"code":0,
-"data":
-[ {
-            "id": "1",
-            "name": "学校管理机构",
-             "persons": [ {
-                "id": "persons1",
-                "name": "党委组织部",
-                "phone": "",
-                "email": "ltzy_zzb@ltzy.edu.cn",
-                "telephone": "0772-3698310"
-            }, {
-                "id": "persons2",
-                "name": "党委办公室",
-                "phone": "",
-                "email": "ltzy_db@ltzy.edu.cn",
-                "telephone": "0772-3679166"
-            }, {
-                "id": "persons3",
-                "name": "党委宣传部",
-                "phone": "",
-                "email": "ltzyxc@163.com",
-                "telephone": "0772-3698220"
-            }, {
-                "id": "persons4",
-                "name": "人事处",
-                "phone": "",
-                "email": "ltzyrsc@163.com",
-                "telephone": "0772-3679185"
-            }, {
-                "id": "persons5",
-                "name": "师资发展科",
-                "phone": "",
-                "email": "",
-                "telephone": "0772-3698225"
-            }, {
-                "id": "persons6",
-                "name": "学校办公室",
-                "phone": "",
-                "email": "ltzy3000@163.com",
-                "telephone": "0772-3698666"
-            } , {
-                "id": "persons7",
-                "name": "教务处",
-                "phone": "",
-                "email": "ltzy_jwc@ltzy.edu.cn",
-                "telephone": "0772-398308"
-            }, {
-                "id": "persons8",
-                "name": "学生处",
-                "phone": "",
-                "email": "ltzyxsc2021@163.com",
-                "telephone": "0772-3679126"
-            }, {
-                "id": "persons9",
-                "name": "保卫处",
-                "phone": "",
-                "email": "ltzybwc@163.com",
-                "telephone": "0772-3679110"
-            }, {
-                "id": "persons10",
-                "name": "科技与图文信息处",
-                "phone": "",
-                "email": "ltzy_kxc@ltzy.edu.cn",
-                "telephone": "0772-3679018"
-            }, {
-                "id": "persons11",
-                "name": "后勤保障处",
-                "phone": [ "0772-3679096" ],
-                "email": "ltzyhqc@163.com",
-                "telephone": "0772-3698253"
-            }, {
-                "id": "persons12",
-                "name": "招生就业处",
-                "phone": [ "0772-3698188","0772-3698108" ],
-                "email": "ltzy_zjc@163.com",
-                "telephone": "0772-3698198"
-            }, {
-                "id": "persons13",
-                "name": "计财处",
-                "phone": "",
-                "email": "ltzy_jcc@ltzy.edu.cn",
-                "telephone": "0772-3698288"
-            }, {
-                "id": "persons14",
-                "name": "纪检监察室",
-                "phone": [ "0772-3679045" ],
-                "email": "ltzyjjjc@163.com",
-                "telephone": "0772-3698221"
-            }, {
-                "id": "persons15",
-                "name": "工会",
-                "phone": "",
-                "email": "1142120293@qq.com",
-                "telephone": "0772-3698187"
-            }, {
-                "id": "persons16",
-                "name": "离退休工作办公室",
-                "phone": [ "0772-3698398" ],
-                "email": "1142120293@qq.com",
-                "telephone": "0772-3698153"
-            }, {
-                "id": "persons17",
-                "name": "团委",
-                "phone": "",
-                "email": "0772-3679102",
-                "telephone": "ltzytw@126.com"
-            }, {
-                "id": "persons18",
-                "name": "质量管理办公室",
-                "phone": "",
-                "email": "ltzyzlb@163.com",
-                "telephone": "0772-3679181"
-            }, {
-                "id": "persons19",
-                "name": "招标采购办公室",
-                "phone": "",
-                "email": "1579391393@qq.com",
-                "telephone": "0772-3698361"
-            }]
-        } ],
-"message":"获取成功"
+  "code": 0,
+  "data": [
+    {
+      "id": "12",
+      "name": "报修电话",
+      "persons": [
+        {
+          "id": "persons33",
+          "name": "紧急报修电话",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-5393335"
+          ]
+        },
+        {
+          "id": "persons34",
+          "name": "空调故障报修",
+          "phone": [
+            "13978281449"
+          ],
+          "email": "",
+          "telephone": [
+            ""
+          ]
+        }
+      ]
+    },
+    {
+      "id": "1",
+      "name": "学校管理机构",
+      "persons": [
+        {
+          "id": "persons1",
+          "name": "党委组织部",
+          "phone": [
+            ""
+          ],
+          "email": "ltzy_zzb@ltzy.edu.cn",
+          "telephone": [
+            "0772-3698310"
+          ]
+        },
+        {
+          "id": "persons2",
+          "name": "党委办公室",
+          "phone": [
+            ""
+          ],
+          "email": "ltzy_db@ltzy.edu.cn",
+          "telephone": [
+            "0772-3679166"
+          ]
+        },
+        {
+          "id": "persons3",
+          "name": "党委宣传部",
+          "phone": [
+            ""
+          ],
+          "email": "ltzyxc@163.com",
+          "telephone": [
+            "0772-3698220"
+          ]
+        },
+        {
+          "id": "persons4",
+          "name": "人事处",
+          "phone": [
+            ""
+          ],
+          "email": "ltzyrsc@163.com",
+          "telephone": [
+            "0772-3679185"
+          ]
+        },
+        {
+          "id": "persons5",
+          "name": "师资发展科",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-3698225"
+          ]
+        },
+        {
+          "id": "persons6",
+          "name": "学校办公室",
+          "phone": [
+            ""
+          ],
+          "email": "ltzy3000@163.com",
+          "telephone": [
+            "0772-3698666"
+          ]
+        },
+        {
+          "id": "persons7",
+          "name": "教务处",
+          "phone": [
+            ""
+          ],
+          "email": "ltzy_jwc@ltzy.edu.cn",
+          "telephone": [
+            "0772-398308"
+          ]
+        },
+        {
+          "id": "persons8",
+          "name": "学生处",
+          "phone": [
+            ""
+          ],
+          "email": "ltzyxsc2021@163.com",
+          "telephone": [
+            "0772-3679126"
+          ]
+        },
+        {
+          "id": "persons9",
+          "name": "保卫处",
+          "phone": [
+            ""
+          ],
+          "email": "ltzybwc@163.com",
+          "telephone": [
+            "0772-3679110"
+          ]
+        },
+        {
+          "id": "persons10",
+          "name": "科技与图文信息处",
+          "phone": [
+            ""
+          ],
+          "email": "ltzy_kxc@ltzy.edu.cn",
+          "telephone": [
+            "0772-3679018"
+          ]
+        },
+        {
+          "id": "persons11",
+          "name": "后勤保障处",
+          "phone": [
+            ""
+          ],
+          "email": "ltzyhqc@163.com",
+          "telephone": [
+            "0772-3698253",
+            "0772-3679096"
+          ]
+        },
+        {
+          "id": "persons12",
+          "name": "招生就业处",
+          "phone": [
+            ""
+          ],
+          "email": "ltzy_zjc@163.com",
+          "telephone": [
+            "0772-3698198",
+            "0772-3698188",
+            "0772-3698108"
+          ]
+        },
+        {
+          "id": "persons13",
+          "name": "计财处",
+          "phone": [
+            ""
+          ],
+          "email": "ltzy_jcc@ltzy.edu.cn",
+          "telephone": [
+            "0772-3698288"
+          ]
+        },
+        {
+          "id": "persons14",
+          "name": "纪检监察室",
+          "phone": [
+            ""
+          ],
+          "email": "ltzyjjjc@163.com",
+          "telephone": [
+            "0772-3698221",
+            "0772-3679045"
+          ]
+        },
+        {
+          "id": "persons15",
+          "name": "工会",
+          "phone": [
+            ""
+          ],
+          "email": "1142120293@qq.com",
+          "telephone": [
+            "0772-3698187"
+          ]
+        },
+        {
+          "id": "persons16",
+          "name": "离退休工作办公室",
+          "phone": [
+            ""
+          ],
+          "email": "1142120293@qq.com",
+          "telephone": [
+            "0772-3698153",
+            "0772-3698398"
+          ]
+        },
+        {
+          "id": "persons17",
+          "name": "团委",
+          "phone": [
+            ""
+          ],
+          "email": "ltzytw@126.com",
+          "telephone": [
+            "0772-3679102"
+          ]
+        },
+        {
+          "id": "persons18",
+          "name": "质量管理办公室",
+          "phone": [
+            ""
+          ],
+          "email": "ltzyzlb@163.com",
+          "telephone": [
+            "0772-3679181"
+          ]
+        },
+        {
+          "id": "persons19",
+          "name": "资助管理办公室",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-3679130"
+          ]
+        },
+        {
+          "id": "persons20",
+          "name": "招标采购办公室",
+          "phone": [
+            ""
+          ],
+          "email": "1579391393@qq.com",
+          "telephone": [
+            "0772-3698361"
+          ]
+        }
+      ]
+    },
+    {
+      "id": "2",
+      "name": "通信信号学院",
+      "persons": [
+        {
+          "id": "persons21",
+          "name": "通信信号学院办公室",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-3698012"
+          ]
+        }
+      ]
+    },
+    {
+      "id": "3",
+      "name": "通信与物联网学院",
+      "persons": [
+        {
+          "id": "persons22",
+          "name": "通信与物联网学院办公室",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-3698003"
+          ]
+        }
+      ]
+    },
+    {
+      "id": "4",
+      "name": "运输管理学院",
+      "persons": [
+        {
+          "id": "persons23",
+          "name": "运输管理学院办公室",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-3698117"
+          ]
+        }
+      ]
+    },
+    {
+      "id": "5",
+      "name": "动力技术学院",
+      "persons": [
+        {
+          "id": "persons24",
+          "name": "动力技术学院办公室",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-3679186"
+          ]
+        }
+      ]
+    },
+    {
+      "id": "6",
+      "name": "城轨交通学院",
+      "persons": [
+        {
+          "id": "persons25",
+          "name": "城轨交通学院办公室",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-3698083"
+          ]
+        }
+      ]
+    },
+    {
+      "id": "7",
+      "name": "建筑技术学院",
+      "persons": [
+        {
+          "id": "persons26",
+          "name": "建筑技术学院办公室",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-3698017"
+          ]
+        }
+      ]
+    },
+    {
+      "id": "8",
+      "name": "汽车技术学院",
+      "persons": [
+        {
+          "id": "persons27",
+          "name": "汽车技术学院办公室",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-3690130"
+          ]
+        }
+      ]
+    },
+    {
+      "id": "9",
+      "name": "智能制造学院",
+      "persons": [
+        {
+          "id": "persons28",
+          "name": "智能制造学院办公室",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-3698087"
+          ]
+        }
+      ]
+    },
+    {
+      "id": "10",
+      "name": "成人教育学院",
+      "persons": [
+        {
+          "id": "persons29",
+          "name": "成人教育学院办公室",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-3698019"
+          ]
+        }
+      ]
+    },
+    {
+      "id": "11",
+      "name": "校外联合办学点",
+      "persons": [
+        {
+          "id": "persons30",
+          "name": "柳州一职校高职部",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-3729035"
+          ]
+        },
+        {
+          "id": "persons31",
+          "name": "柳州二职校高职部",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0772-2628691"
+          ]
+        },
+        {
+          "id": "persons32",
+          "name": "南宁四职校高职部",
+          "phone": [
+            ""
+          ],
+          "email": "",
+          "telephone": [
+            "0771-5321566"
+          ]
+        }
+      ]
+    }
+  ],
+  "message": "获取成功"
 }
