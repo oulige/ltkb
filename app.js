84b9b671dddf69918427df2522b808b04a491b67
oulige <103018846+oulige@users.noreply.github.com>
2022-09-03 18:24:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,84 @@
           "navigateTo": "/pages/xyfj/index"
       }
   ],
+  "jxl": [
+   {
+          start: "08:40",
+          stop: "09:20"
+        }, {
+          start: "09:30",
+          stop: "10:10"
+        }, {
+          start: "10:40",
+          stop: "11:20"
+        }, {
+          start: "11:30",
+          stop: "12:10"
+        }, {
+          start: "14:30",
+          stop: "15:10"
+        }, {
+          start: "15:20",
+          stop: "16:00"
+        }, {
+          start: "16:20",
+          stop: "17:00"
+        }, {
+          start: "17:00",
+          stop: "17:50"
+        }, {
+          start: "19:00",
+          stop: "19:40"
+        }, {
+          start: "19:50",
+          stop: "20:30"
+        }, {
+            start: "20:40",
+            stop: "21:20"
+          }, {
+            start: "21:30",
+            stop: "22:10"
+          }
+  ],
+  "sxl": [
+   {
+          start: "08:30",
+          stop: "09:10"
+        }, {
+          start: "09:20",
+          stop: "10:00"
+        }, {
+          start: "10:30",
+          stop: "11:10"
+        }, {
+          start: "11:20",
+          stop: "12:00"
+        }, {
+          start: "14:30",
+          stop: "15:10"
+        }, {
+          start: "15:20",
+          stop: "16:00"
+        }, {
+          start: "16:20",
+          stop: "17:00"
+        }, {
+          start: "17:10",
+          stop: "17:50"
+        }, {
+          start: "19:00",
+          stop: "19:40"
+        }, {
+          start: "19:50",
+          stop: "20:30"
+          }, {
+            start: "20:40",
+            stop: "21:20"
+          }, {
+            start: "21:30",
+            stop: "22:10"
+          }
+  ],
   "cjurl": "https://pkwx.ltzy.edu.cn/wx/scores?code=Z716sKgtybFfXomWtvx1QXjkepRro&cardNumber=",
   "kburl": "https://pkwx.ltzy.edu.cn/wx/corseuid?code=Z716sKgtybFfXomWtvx1QXjkepRro&zc=",
   "xiaoli":"备注:\n1. 开学第一周：8月29日行政人员、教师上班，8月30（星期二）正式上课，新生报到时间待定。\n2. 国庆节：10月1日-7日放假调休，10月8日-9日上班；中秋节：9月10日-12日放假；元旦：2022年12月31-2023年1月2日放假。\n3. 第十周星期五、六（11月4、5日）举行学校第二十届运动会。\n4. 寒假6周（1月10日至2月26日），1月22日春节，2月5日元宵节。\n5. 每周二下午，教师进行政治学习和业务学习，学生进行政治学习、主题班会、主题团日活动等。"
