b0ac109535c4508b64ba55b12b3537466db3c6cb
oulige <103018846+oulige@users.noreply.github.com>
2022-09-06 21:57:16 +0800

    Update xsxz.js

--- xsxz.js
+++ xsxz.js
@@ -1,11 +1,7 @@
 {
   "code": "0",
   "imgList": [
-    {"pic":"https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3mzvvwgj20tz16athk.jpg","name":""},
-      {
-        "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3na4klfj20tz16lk33.jpg", "name": "" },
-      {
-        "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3nju4h7j20tz16i49d.jpg", "name": "" },
+  
       { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3o4p3tjj20tz16i7bz.jpg", "name": "" },
       { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5q38nijlxj20sk178gqa.jpg", "name": ""  },
       { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5q33a3d9qj20sm17ogqo.jpg", "name": ""  },
