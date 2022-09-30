d3c396a6ea3bf46bc07e3ca418274af858f3d1f9
oulige <103018846+oulige@users.noreply.github.com>
2022-09-30 20:59:48 +0800

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,27 @@
+[
+  {
+    "applicationName": "校园电话",
+    "url": "/pages/txl/txl",
+    "description": "校园常用电话本",
+    "imageURL": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i43yiei8j2046046aa3.jpg",
+    "enabled": true,
+    "createdAt": "2022-07-18T08:19:04.618Z"
+  },
+  {
+    "applicationName": "校园风景",
+    "url": "/pages/xyfj/index",
+    "description": "校园图片欣赏",
+    "imageURL": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i43yiei8j2046046aa3.jpg",
+    "enabled": true,
+    "createdAt": "2022-07-18T08:49:18.829Z"
+  },
+   {
+    "applicationName": "常用网站",
+    "url": "/pages/link/link",
+    "description": "常用网站快速复制浏览",
+    "imageURL": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i43yiei8j2046046aa3.jpg",
+    "enabled": true,
+    "createdAt": "2022-07-18T08:49:18.829Z"
+  }
+  
+]
