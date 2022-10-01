9b243bd637db8547c44bf9c0a0c02aa0e551857e
oulige <103018846+oulige@users.noreply.github.com>
2022-10-01 15:28:41 +0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -16,6 +16,14 @@
     "createdAt": "2022-07-18T08:49:18.829Z"
   },
    {
+    "applicationName": "时间计时",
+    "url": "/pages/day/day",
+    "description": "看一下离放假还有多少天",
+    "imageURL": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i43yiei8j2046046aa3.jpg",
+    "enabled": true,
+    "createdAt": "2022-07-18T08:49:18.829Z"
+  },
+   {
     "applicationName": "常用网站",
     "url": "/pages/link/link",
     "description": "常用网站快速复制浏览",
