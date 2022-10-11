da968ebb67f41dead404f3f116d83429868ff1a3
oulige <103018846+oulige@users.noreply.github.com>
2022-10-11 21:35:32 +0800

    Create cate.js

---
+++ cate.js
@@ -0,0 +1,62 @@
+{
+  "status": 1,
+  "msg": "success",
+  "data": [
+    {
+      "name": "xw",
+      "text": "新闻"
+    },
+    {
+      "name": "mil",
+      "text": "军事"
+    },
+    {
+      "name": "tech",
+      "text": "科技"
+    },
+    {
+      "name": "finance",
+      "text": "股票"
+    },
+    {
+      "name": "money",
+      "text": "财经"
+    },
+    {
+      "name": "sports",
+      "text": "体育"
+    },
+    {
+      "name": "ent",
+      "text": "娱乐"
+    },
+    {
+      "name": "edu",
+      "text": "教育"
+    },
+    {
+      "name": "ast",
+      "text": "星座"
+    },
+    {
+      "name": "eladies",
+      "text": "女性"
+    },
+    {
+      "name": "travel",
+      "text": "旅游"
+    },
+    {
+      "name": "baby",
+      "text": "育儿"
+    },
+    {
+      "name": "health",
+      "text": "健康"
+    },
+    {
+      "name": "fashion",
+      "text": "时尚"
+    }
+  ]
+}
