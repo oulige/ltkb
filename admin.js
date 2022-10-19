40bc4527192dce872283c2e3f263b064c094001e
oulige <103018846+oulige@users.noreply.github.com>
2022-10-19 14:35:52 +0800

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,118 @@
+{
+  "code": "0",
+  "notice": "新上线一波功能，请前往【菜单】测试使用",
+  "version":1.03,
+  "pic": [
+    {
+      "picUrl": "https://dd-static.jd.com/ddimg/jfs/t1/41400/9/19908/228191/634b9c3dE2b24159b/69a78e9f881c7e83.png",
+      "navigateTo": "/pages/pingjiao/pingjiao"
+    },
+    {
+      "picUrl": "https://tuepic.com/i/2022/10/12/634683978e478.png",
+      "navigateTo": "/pages/daka/daka"
+    }
+  ],
+  "jxl": [
+    {
+      "start": "08:40",
+      "stop": "09:20"
+    },
+    {
+      "start": "09:30",
+      "stop": "10:10"
+    },
+    {
+      "start": "10:40",
+      "stop": "11:20"
+    },
+    {
+      "start": "11:30",
+      "stop": "12:10"
+    },
+    {
+      "start": "14:30",
+      "stop": "15:10"
+    },
+    {
+      "start": "15:20",
+      "stop": "16:00"
+    },
+    {
+      "start": "16:20",
+      "stop": "17:00"
+    },
+    {
+      "start": "17:00",
+      "stop": "17:50"
+    },
+    {
+      "start": "19:00",
+      "stop": "19:40"
+    },
+    {
+      "start": "19:50",
+      "stop": "20:30"
+    },
+    {
+      "start": "20:40",
+      "stop": "21:20"
+    },
+    {
+      "start": "21:30",
+      "stop": "22:10"
+    }
+  ],
+  "sxl": [
+    {
+      "start": "08:30",
+      "stop": "09:10"
+    },
+    {
+      "start": "09:20",
+      "stop": "10:00"
+    },
+    {
+      "start": "10:30",
+      "stop": "11:10"
+    },
+    {
+      "start": "11:20",
+      "stop": "12:00"
+    },
+    {
+      "start": "14:30",
+      "stop": "15:10"
+    },
+    {
+      "start": "15:20",
+      "stop": "16:00"
+    },
+    {
+      "start": "16:20",
+      "stop": "17:00"
+    },
+    {
+      "start": "17:10",
+      "stop": "17:50"
+    },
+    {
+      "start": "19:00",
+      "stop": "19:40"
+    },
+    {
+      "start": "19:50",
+      "stop": "20:30"
+    },
+    {
+      "start": "20:40",
+      "stop": "21:20"
+    },
+    {
+      "start": "21:30",
+      "stop": "22:10"
+    }
+  ],
+  "cjurl": "https://pkwx.ltzy.edu.cn/wx/scores?code=Z716sKgtybFfXomWtvx1QXjkepRro&cardNumber=",
+  "kburl": "https://pkwx.ltzy.edu.cn/wx/corseuid?code=Z716sKgtybFfXomWtvx1QXjkepRro&zc=",
+  "bbq": ""
+}
