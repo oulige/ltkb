17c1cbc9d02843c00bf3a0d0e36a26e599b2a415
oulige <103018846+oulige@users.noreply.github.com>
2022-12-02 14:40:46 +0800

    Create help.js

---
+++ help.js
@@ -0,0 +1,10 @@
+{
+  "code": "0",
+  "imgList": [
+  
+      { "pic": "https://ldbbs.ldmnq.com/bbs/topic/attachment/2022-12/e381ca56-b945-4732-968b-5b3df2f604ff.jpg", "name": "点击二维码长按识别" },
+      { "pic": "https://ldbbs.ldmnq.com/bbs/topic/attachment/2022-12/77cd7191-06a0-4491-9523-da408f165351.jpg", "name": "跳转链接（提示用户不存在不用理会）"  },
+      { "pic": "https://ldbbs.ldmnq.com/bbs/topic/attachment/2022-12/b237711f-60ed-4234-977e-735032c34ec4.jpg", "name": "复制链接"  },
+      { "pic": "https://ldbbs.ldmnq.com/bbs/topic/attachment/2022-12/16b422c8-a7df-49e2-9978-7603987d8a22.jpg", "name": "粘贴链接登录即可"  }
+  ]
+}
