c69701b180ac5f46313c37057d666bd4b8024619
oulige <103018846+oulige@users.noreply.github.com>
2022-12-05 20:19:49 +0800

    Create helps.js

---
+++ helps.js
@@ -0,0 +1,10 @@
+{
+  "code": "0",
+  "imgList": [
+  
+      { "pic": "https://ldbbs.ldmnq.com/bbs/topic/attachment/2022-12/127fd9d0-46e2-41e1-a6c3-fa49051fa8db.jpg", "name": "1.点击二维码保存图片" },
+      { "pic": "https://ldbbs.ldmnq.com/bbs/topic/attachment/2022-12/f6facbac-47e9-4989-80b9-483076991869.jpg", "name": "2.用企业微信扫一扫保存的二维码"  },
+      { "pic": "https://ldbbs.ldmnq.com/bbs/topic/attachment/2022-12/01b4e16c-4199-41b0-8c31-a6b603c63fdf.jpg", "name": "3.复制扫码的链接"  },
+      { "pic": "https://ldbbs.ldmnq.com/bbs/topic/attachment/2022-12/d5c87cc1-09f2-440d-9e16-b7ff96c1792a.jpg", "name": "4.粘贴链接登录即可"  }
+  ]
+}
