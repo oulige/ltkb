2f6696887ba40ba92a32afae255808d5b0656cfe
oulige <103018846+oulige@users.noreply.github.com>
2023-02-20 13:42:22 +0800

    Update xsxz.js

--- xsxz.js
+++ xsxz.js
@@ -5,7 +5,7 @@
       { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3o4p3tjj20tz16i7bz.jpg", "name": "" },
       { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5q38nijlxj20sk178gqa.jpg", "name": ""  },
       { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5q33a3d9qj20sm17ogqo.jpg", "name": ""  },
-      { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5q3618x9tj20ss1coq99.jpg", "name": ""  },
+      { "pic": "https://ldbbs.ldmnq.com/bbs/topic/attachment/2023-2/c711dee8-a517-4486-8e4a-8cf1f0ad3925.png", "name": ""  },
       { "pic": "https://tva1.sinaimg.cn/large/e6c9d24egy1h5i3u1sw20j20k80nidjl.jpg", "name": "学校地图" }
   ]
 }
